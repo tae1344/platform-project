48f15de7957f6c1aa123f43381141eb6a598df48
tae1344 <xodus1344@gmail.com>
2024-01-14 21:02:30 +0900

    중간커밋

--- client/src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello, Dashboard Page!</h1>
-      <p></p>
-    </div>
-  );
-}
--- client/src/app/template.tsx
+++
@@ -1,8 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <div>{children}</div>
-      <p>template.tsx</p>
-    </div>
-  );
-}
